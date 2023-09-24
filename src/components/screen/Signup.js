23eb4a4dbfe908800d8865a74952ef2e9f017cea
jaanvi02 <89679703+jaanvi02@users.noreply.github.com>
2023-09-25 00:53:47 +0530

    Update Signup.js

--- src/components/screen/Signup.js
+++ src/components/screen/Signup.js
@@ -6,7 +6,7 @@ export default function Signup() {
     const[credentials,setcredentials]=useState({name:"",Email:"",Password:"",location:""})
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:3000/api/createuser", {
+        const response = await fetch("http://deploy-mern-api-alpha.vercel.app/createuser", {
           method: 'POST',
           headers: {
             'Content-type': 'application/json'
