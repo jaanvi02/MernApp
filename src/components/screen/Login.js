ed43c42f6cd513379601b39de4a8491c8bc53572
jaanvi02 <89679703+jaanvi02@users.noreply.github.com>
2023-09-25 00:51:49 +0530

    Update Login.js

--- src/components/screen/Login.js
+++ src/components/screen/Login.js
@@ -6,7 +6,7 @@ export default function Login() {
   const [alertVisible, setAlertVisible] = useState(false);
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:3000/api/loginuser", {
+        const response = await fetch("https://deploy-mern-api-alpha.vercel.app/loginuser", {
           method: 'POST',
           headers: {
             'Content-type': 'application/json'
